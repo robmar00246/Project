ba5b7e9b22c92ef1925000ebea4aba00d1e19f2b
robmar00246 <robmar00246@stud.noroff.no>
2024-04-30 18:00:29 +0200

    render test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fileUpload = require('express-fileupload');
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var picturesRouter = require('./routes/pictures');
